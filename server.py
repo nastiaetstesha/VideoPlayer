5cdfc1da950fbfcf93098e182ae6304fe0255367
Avakova Anastasiia <avakova.ast@gmail.com>
2025-02-03 15:41:09 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,9 @@
+from livereload import Server
+
+server = Server()
+
+server.watch('/Users/egorsemin/Practice/video-player-jslib-master/*.html')
+server.watch('/Users/egorsemin/Practice/video-player-jslib-master/*.css')
+server.watch('/Users/egorsemin/Practice/video-player-jslib-master/*.js')
+
+server.serve(root='/Users/egorsemin/Practice/video-player-jslib-master', default_filename='index.html')
